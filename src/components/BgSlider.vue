ff0d86ff7b684327a5d0dddf7b2a2d76af4fbf02
Mavlon4658 <mavlonbe4658>
2025-04-23 19:56:39 +0500

    project is ready

---
+++ src/components/BgSlider.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="bg-slider">
+        <swiper
+            :slides-per-view="1"
+            :space-between="50"
+            :loop="true"
+            :speed="1000"
+            :effect="'fade'"
+            :autoplay="{
+                delay: 4000,
+                disableOnInteraction: false,
+            }"
+            :allowTouchMove="false"
+            :modules="modules"
+            @swiper="onSwiper"
+            @slideChange="onSlideChange"
+        >
+            <swiper-slide>
+                <img src="@/assets/img/slide-1.jpg" alt="">
+            </swiper-slide>
+            <swiper-slide>
+                <img src="@/assets/img/slide-2.jpg" alt="">
+            </swiper-slide>
+            <swiper-slide>
+                <img src="@/assets/img/slide-3.jpg" alt="">
+            </swiper-slide>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+
+import {EffectFade, Autoplay } from 'swiper/modules';
+
+export default {
+    name: "BgSlider",
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {};
+        const onSlideChange = () => {};
+        return {
+            onSwiper,
+            onSlideChange,
+            modules: [EffectFade, Autoplay],
+        };
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header class="header">
+        <div class="container header-container">
+            <router-link to="/" class="header-logo">
+                <img src="@/assets/img/header-logo.svg" alt="">
+                <span>The Garden Gurus</span>
+            </router-link>
+            <ul class="header-navs">
+                <li>
+                    <a href="#" class="header-navs__link">
+                        <i class="fa-regular fa-paper-plane"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="header-navs__link">
+                        <i class="fa-solid fa-share-nodes"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="header-navs__link">
+                        <i class="fa-regular fa-envelope"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="header-navs__link">
+                        <i class="fa-solid fa-phone"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+
+</style>
